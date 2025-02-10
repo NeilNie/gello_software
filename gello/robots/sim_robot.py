ebd7698b87de5ca5906f6526a2d691a34d34fa2a
Neil Nie <yongyang.nie@gmail.com>
2025-02-09 20:37:00 -0800

    update bimanual panda code

--- gello/robots/sim_robot.py
+++ gello/robots/sim_robot.py
@@ -51,20 +51,20 @@ def attach_hand_to_arm(
     attachment_site.attach(hand_mjcf)
 
 
-def build_scene(robot_xml_path: str, gripper_xml_path: Optional[str] = None):
+def build_scene(robot_xml_path: str, gripper_xml_path: Optional[str] = None, num_robots: int = 1):
     # assert robot_xml_path.endswith(".xml")
 
     arena = mjcf.RootElement()
-    arm_simulate = mjcf.from_path(robot_xml_path)
-    # arm_copy = mjcf.from_path(xml_path)
+    for _ in range(num_robots):
+        arm_simulate = mjcf.from_path(robot_xml_path)
+        # arm_copy = mjcf.from_path(xml_path)
 
-    if gripper_xml_path is not None:
-        # attach gripper to the robot at "attachment_site"
-        gripper_simulate = mjcf.from_path(gripper_xml_path)
-        attach_hand_to_arm(arm_simulate, gripper_simulate)
+        if gripper_xml_path is not None:
+            # attach gripper to the robot at "attachment_site"
+            gripper_simulate = mjcf.from_path(gripper_xml_path)
+            attach_hand_to_arm(arm_simulate, gripper_simulate)
 
-    arena.worldbody.attach(arm_simulate)
-    # arena.worldbody.attach(arm_copy)
+        arena.worldbody.attach(arm_simulate)
 
     return arena
 
@@ -242,6 +242,133 @@ class MujocoRobotServer:
                 # Rudimentary time keeping, will drift relative to wall clock.
                 time_until_next_step = self._model.opt.timestep - (
                     time.time() - step_start
+                )
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
+
+    def stop(self) -> None:
+        self._zmq_server_thread.join()
+
+    def __del__(self) -> None:
+        self.stop()
+
+
+class MujocoBimanualRobotServer:
+    def __init__(
+            self,
+            xml_path: str,
+            gripper_xml_path: Optional[str] = None,
+            host: str = "127.0.0.1",
+            port: int = 5556,
+            print_joints: bool = False,
+    ):
+        self._has_gripper = gripper_xml_path is not None
+        arena = build_scene(xml_path, gripper_xml_path, num_robots=1)
+
+        assets: Dict[str, str] = {}
+        for asset in arena.asset.all_children():
+            if asset.tag == "mesh":
+                f = asset.file
+                assets[f.get_vfs_filename()] = asset.file.contents
+
+        xml_string = arena.to_xml_string()
+        # save xml_string to file
+        with open("arena.xml", "w") as f:
+            f.write(xml_string)
+
+        # left arm
+        self._model_l = mujoco.MjModel.from_xml_string(xml_string, assets)
+        self._data_l = mujoco.MjData(self._model_l)
+
+        # right arm
+        self._model_r = mujoco.MjModel.from_xml_string(xml_string, assets)
+        self._data_r = mujoco.MjData(self._model_r)
+
+        self._num_joints = self._model_l.nu + self._model_r.nu
+
+        self._joint_state = np.zeros(self._num_joints)
+        self._joint_cmd = self._joint_state
+
+        self._zmq_server = ZMQRobotServer(robot=self, host=host, port=port)
+        self._zmq_server_thread = ZMQServerThread(self._zmq_server)
+
+        self._print_joints = print_joints
+
+    def num_dofs(self) -> int:
+        return self._num_joints
+
+    def get_joint_state(self) -> np.ndarray:
+        return self._joint_state
+
+    def command_joint_state(self, joint_state: np.ndarray) -> None:
+        assert len(joint_state) == self._num_joints, (
+            f"Expected joint state of length {self._num_joints}, "
+            f"got {len(joint_state)}."
+        )
+        _joint_state = joint_state.copy()
+        _joint_state[-1] = 255 - _joint_state[-1] * 255
+        self._joint_cmd = _joint_state
+
+    def freedrive_enabled(self) -> bool:
+        return True
+
+    def set_freedrive_mode(self, enable: bool):
+        pass
+
+    def get_observations(self) -> Dict[str, np.ndarray]:
+        joint_positions = self._data.qpos.copy()[: self._num_joints]
+        joint_velocities = self._data.qvel.copy()[: self._num_joints]
+        ee_site = "attachment_site"
+        try:
+            ee_pos = self._data.site_xpos.copy()[
+                mujoco.mj_name2id(self._model, 6, ee_site)
+            ]
+            ee_mat = self._data.site_xmat.copy()[
+                mujoco.mj_name2id(self._model, 6, ee_site)
+            ]
+            ee_quat = np.zeros(4)
+            mujoco.mju_mat2Quat(ee_quat, ee_mat)
+        except Exception:
+            ee_pos = np.zeros(3)
+            ee_quat = np.zeros(4)
+            ee_quat[0] = 1
+        gripper_pos = self._data.qpos.copy()[self._num_joints - 1]
+        return {
+            "joint_positions": joint_positions,
+            "joint_velocities": joint_velocities,
+            "ee_pos_quat": np.concatenate([ee_pos, ee_quat]),
+            "gripper_position": gripper_pos,
+        }
+
+    def serve(self) -> None:
+        # start the zmq server
+        self._zmq_server_thread.start()
+        with mujoco.viewer.launch_passive(self._model_l, self._data_l) as viewer:
+            while viewer.is_running():
+                step_start = time.time()
+
+                # mj_step can be replaced with code that also evaluates
+                # a policy and applies a control signal before stepping the physics.
+                self._data_l.ctrl[:] = self._joint_cmd
+                mujoco.mj_step(self._model, self._data_l)
+                self._joint_state = self._data_l.qpos.copy()[: self._num_joints]
+
+                if self._print_joints:
+                    print(self._joint_state)
+
+                # Example modification of a viewer option: toggle contact points every two seconds.
+                with viewer.lock():
+                    # TODO remove?
+                    viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(
+                        self._data_l.time % 2
+                    )
+
+                # Pick up changes to the physics state, apply perturbations, update options from GUI.
+                viewer.sync()
+
+                # Rudimentary time keeping, will drift relative to wall clock.
+                time_until_next_step = self._model.opt.timestep - (
+                        time.time() - step_start
                 )
                 if time_until_next_step > 0:
                     time.sleep(time_until_next_step)
